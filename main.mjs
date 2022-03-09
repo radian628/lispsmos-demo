8e49e539921555b73a95490a62e6216ddebeb5a6
radian628 <break.a.radian@gmail.com>
2022-03-08 16:48:01 -0800

    added comments to js file

--- main.mjs
+++ main.mjs
@@ -3,8 +3,14 @@ import * as fs from "node:fs/promises";
 import * as http from "node:http";
 
 async function build() {
+  //Create the lispsmos compiler.
   let lc = new compiler.LispsmosCompiler();
+  
+  //Register some useful utility macros
   utilityMacros.register(lc);
+
+  //Register an "importer function" that tells LISPsmos how to get external source files.
+  //In this case, it's getting them from the filesystem.
   lc.registerImporter(async (lc, location) => {
     try {
       let sourceCode = await fs.readFile(location);
@@ -13,17 +19,27 @@ async function build() {
       return { payload: undefined, success: false };
     }
   }); 
+
+  //The file main.lisp is our entry point. We're starting the compilation process from there.
   let sourceCode = await fs.readFile("./main.lisp");
+
+  //Compile the LISPsmos into a Desmos graph state object.
   let result = await lc.compile(sourceCode.toString());
+
+  //Return the graph.
   return result;
 }
 
 
 let server = http.createServer(async (req, res) => {
+    //Set headers so we can receive an HTTP request in Desmos.
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+
+    //Send the LISPsmos code to the requester.
     res.end(JSON.stringify(await build()));
-    return;
 });
 
+
+//Run the web server on port 8081.
 server.listen("8081", "localhost"); 
